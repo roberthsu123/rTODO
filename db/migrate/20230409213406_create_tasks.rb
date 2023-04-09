30afbe9d1f9eaa869f1716abd753a20af66afcde
robert <robert@ubuntu.myguest.virtualbox.org>
2023-04-09 14:50:58 -0700

    added not null constraint

--- db/migrate/20230409213406_create_tasks.rb
+++ db/migrate/20230409213406_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :description
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_213406) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
